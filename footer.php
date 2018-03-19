0282c21a349a187586d61b1989c1af7963db0371
jimjimbojames <herman.james@gmail.com>
2018-03-19 17:26:46 -0500

    adding loading text

--- footer.php
+++ footer.php
@@ -4,7 +4,19 @@
 		<footer>
 			<!-- <p>This is the footer</p> -->
 			<?php 
-			  // wp_nav_menu(array('theme_location'=>'secondary'));  ?>
+			  // wp_nav_menu(array('theme_location'=>'secondary'));  
+
+
+
+				global $wp_query; // you can remove this line if everything works for you
+ 
+				// don't display the button if there are not enough posts
+				if (  $wp_query->max_num_pages > 1 )
+				echo '<div class="loading">Loading</div>'; // you can use <a> as well
+
+
+
+			?>
 		</footer>
 	
 	
